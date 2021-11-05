3bf54cf73a5b8c982efec743fecbe166e0f24333
iidaissei <issei.288.higashi@icloud.com>
2021-11-05 13:05:04 +0900

    first commit

---
+++ src/obj_tf_listener.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python  
+import rospy
+
+import math
+import tf2_ros
+import geometry_msgs.msg
+
+if __name__ == '__main__':
+    rospy.init_node('obj_tf_listener')
+
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        try:
+            trans = tfBuffer.lookup_transform('fixed_human', 'map', rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
+            continue
+        rate.sleep()
